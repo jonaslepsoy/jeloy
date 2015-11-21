5da961c48490865c95e5de377f11ae7e0aac382b
Thomas Bergheim <thomas.bergheim@bouvet.no>
2015-11-21 15:27:54 +0100

    Beacons should update themselves

--- src/beacons.js
+++ src/beacons.js
@@ -8,19 +8,43 @@ var beaconNames = [
   'Shanghai'
 ];
 
-// Init teams and the server
-function init() {
+beaconNames.forEach(function(beacon) {
+  beacons[beacon] = {score: 0, cappers: []};
+});
 
-  beaconNames.forEach(function(beacon) {
-      beacons[beacon] = {score: 0, cappers: []};
-  });
+function update(teams) {
+  var t = new Date();
 
-  console.log(beacons);
+  for (var beacon in beacons.items){
+    console.log(beacon);
+    var blue = 0;
+    var red = 0;
+    for(var i = 0;i < beacons.items[beacon].cappers.length;i++){
+      console.log(beacons.items[beacon].cappers[i]);
+      console.log('Red: ', teams.red.players.indexOf(beacons.items[beacon].cappers[i]) > -1);
+      console.log('Blue: ', teams.blue.players.indexOf(beacons.items[beacon].cappers[i]) > -1);
+
+      if(teams.red.players.indexOf(beacons.items[beacon].cappers[i]) > -1){
+        red++;
+      }
+      if (teams.blue.players.indexOf(beacons.items[beacon].cappers[i]) > -1){
+          blue++;
+      }
+    }
+    if(red > blue){
+      beacons.items[beacon].score++;
+    } else if (red < blue) {
+      beacons.items[beacon].score--;
+    }
+    console.log('Score for ' + beacon + ' is ' + beacons.items[beacon].score);
+
+    // We have counted the score for this tick, reset cap
+    beacons.items[beacon].cappers = [];
+  }
+
 }
 
-init();
-
-
 module.exports = {
-    items: beacons
+    items: beacons,
+    update: update
 }
